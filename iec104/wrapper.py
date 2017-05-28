bda8d138fc55c60b6ed5bf47231ba2441ca4eb71
Thomas <thomast3@gmx.de>
2017-05-28 17:49:46 +0200

    Added IOA
    For SQ = 0 only

--- iec104/wrapper.py
+++ iec104/wrapper.py
@@ -29,14 +29,15 @@ class IEC104Wrapper():
             return "ERROR: APDU too long."
         return start + struct.pack("B", len(apdu))
 
-    def create_apdu(self, frame, asdu_type, cot, originator_address, common_address, message, ssn = 0, rsn = 0):
+    def create_apdu(self, frame, asdu_type, cause_of_transmission, originator_address, common_address, information_object_address, message, ssn = 0, rsn = 0):
         """
-        Creates a IEC104 APDU without header based on the given parameters.
+        Creates a IEC104 APDU without header.
         :param frame: Frame format to be used.
         :param asdu_type: Type of the message as string.
-        :param cot: A tuple containing the cause of transmission as string(e.g. periodic), P/N Bit(defaults to 0) and Testbit(defaults to 0).
+        :param cause_of_transmission: A tuple containing the cause of transmission as string(e.g. periodic), P/N Bit(defaults to 0) and Testbit(defaults to 0).
         :param originator_address: Originator address as integer.
         :param common_address: Common address of ASDUs as integer.
+        :param information_object_address: Information object address as integer.
         :param message: Message to be wrapped.
         :param ssn: Send sequence number. This is also used to store relevant information for the U-Frame(has to contain the function name and type: e.g. "test-con").
         :param rsn: Receive sequence number.
@@ -45,7 +46,7 @@ class IEC104Wrapper():
         apci = self.wrap_frame(frame, ssn, rsn)
         if type(apci) is str:
             return apci
-        asdu = self.wrap_asdu(asdu_type, cot, originator_address, common_address, message)
+        asdu = self.wrap_asdu(asdu_type, cause_of_transmission, originator_address, common_address, information_object_address, message)
         if type(asdu) is str:
             return asdu
         return apci + asdu
@@ -118,32 +119,39 @@ class IEC104Wrapper():
             print("Warning: U-Frame was made without an active function.")
         return struct.pack('<2BH', byte, 0x00, 0x00)
     
-    def wrap_asdu(self, asdu_type, cot, originator_address, common_address, message):
+    def wrap_asdu(self, asdu_type, cause_of_transmission, originator_address, common_address, information_object_address, message):
         """
-        Adds a IEC 104 ASDU to a struct based on the given parameters.
+        Adds a IEC 104 ASDU to a struct.
         :param asdu_type: Type of the message as string.
-        :param cot: A tuple containing the cause of transmission as string(e.g. periodic), P/N Bit(defaults to 0) and Testbit(defaults to 0).
+        :param cause_of_transmission: A tuple containing the cause of transmission as string(e.g. periodic), P/N Bit(defaults to 0) and Testbit(defaults to 0).
         :param originator_address: Originator address as integer.
         :param common_address: Common address of ASDUs as integer.
+        :param information_object_address: Information object address as integer.
         :param message: Message to be wrapped.
         :return: Struct containing an IEC 104 ASDU as bytestring. ERROR if failed.
         """
         type_id = self.wrap_asdu_type(asdu_type)
         if type(type_id) is str:
             return type_id
-        cot = self.wrap_cause_of_transmission(cot, originator_address)
+        vsq = self.wrap_variable_structure_qualifier(type_id)
+        if type(vsq) is str:
+            return type_id
+        cot = self.wrap_cause_of_transmission(cause_of_transmission, originator_address)
         if type(cot) is str:
             return cot
         ca = self.wrap_common_address(common_address)
         if type(ca) is str:
             return ca
-        return type_id + cot + ca
+        ioa = self.wrap_information_object_address(information_object_address)
+        if type(ioa) is str:
+            return ioa
+        return struct.pack('<B', type_id) + vsq + cot + ca + ioa
 
     def wrap_asdu_type(self, asdu_type):
         """
-        Adds a IEC 104 type identification to a struct based on the given ASDU type.
+        Finds the type identification corresponding to the given ASDU type.
         :param asdu_type: Type of the message as string.
-        :return: Struct containing an IEC 104 type identification as bytestring. ERROR if failed.
+        :return: Type identification as integer. ERROR if failed.
         """
         type_id = "ERROR: The ASDU type was not recognized."
         if asdu_type == 'M_BO_NA_1':
@@ -156,10 +164,7 @@ class IEC104Wrapper():
             type_id = 100
         if asdu_type == 'C_RD_NA_1':
             type_id = 102
-        if type(type_id) is str:
-             return type_id
-        vsq = self.wrap_variable_structure_qualifier(type_id)
-        return struct.pack('<B', type_id) + vsq
+        return type_id
 
     def wrap_variable_structure_qualifier(self, type_id):
         """
@@ -177,35 +182,35 @@ class IEC104Wrapper():
              return vsq
         return struct.pack('<B', vsq)
 
-    def wrap_cause_of_transmission(self, cot, originator_address):
+    def wrap_cause_of_transmission(self, cause_of_transmission, originator_address):
         """
         Adds a IEC 104 cause of transmission and originator address to a struct based on the type identification.
-        :param cot: A tuple containing the cause of transmission as string(e.g. periodic), P/N Bit(defaults to 0) and Testbit(defaults to 0).
+        :param cause_of_transmission: A tuple containing the cause of transmission as string(e.g. periodic), P/N Bit(defaults to 0) and Testbit(defaults to 0).
         :param originator_address: Originator address as integer.
         :return: Struct containing an IEC 104 cause of transmission and originator address as bytestring. ERROR if failed.
         """
         cause = "ERROR: No cause of transmission was found."
         pn = 0
         test = 0
-        if not type(cot[0]) is str:
+        if not type(cause_of_transmission[0]) is str:
             return "ERROR: Cause of transmission has to be a string."
-        if ("periodic" in cot[0]) or ("cyclic" in cot[0]):
+        if ("periodic" in cause_of_transmission[0]) or ("cyclic" in cause_of_transmission[0]):
             cause = 1
-        if "spontaneous" in cot[0]:
+        if "spontaneous" in cause_of_transmission[0]:
             cause = 3
-        if ("request" in cot[0]) or ("requested" in cot[0]):
+        if ("request" in cause_of_transmission[0]) or ("requested" in cause_of_transmission[0]):
             cause = 5
-        if "activation" in cot[0]:
+        if "activation" in cause_of_transmission[0]:
             cause = 6
-        if "activation confirmation" in cot[0]:
+        if "activation confirmation" in cause_of_transmission[0]:
             cause = 7
-        if ("return information" in cot[0]) and ("remote command" in cot[0]):
+        if ("return information" in cause_of_transmission[0]) and ("remote command" in cause_of_transmission[0]):
             cause = 11
         if type(cause) is str:
             return cause
-        if cot[1] == 1:
+        if cause_of_transmission[1] == 1:
             pn = 64
-        if cot[2] == 1:
+        if cause_of_transmission[2] == 1:
             test = 128
         if (not type(originator_address) is int) or (originator_address < 0) or (originator_address > 255):
             return "ERROR: Originator address has to be an integer between 0 and 255."
@@ -213,7 +218,7 @@ class IEC104Wrapper():
 
     def wrap_common_address(self, common_address):
         """
-        Adds a IEC 104 common address to a struct based on the given parameters.
+        Adds a IEC 104 common address to a struct.
         :param common_address: Common address of ASDUs as integer.
         :param message: Message to be wrapped.
         :return: Struct containing an IEC 104 common address as bytestring. ERROR if failed.
@@ -221,6 +226,16 @@ class IEC104Wrapper():
         if (not type(common_address) is int) or (common_address < 1) or (common_address > 65535):
             return "ERROR: Common address has to be an integer between 1 and 65535."
         return struct.pack('<2B', common_address & 0xFF, (common_address >> 8) & 0xFF)
+
+    def wrap_information_object_address(self, information_object_address):
+        """
+        Adds a IEC 104 information object address to a struct.
+        :param information_object_address: Information object address as integer.
+        :return: Struct containing an IEC 104 information object address as bytestring. ERROR if failed.
+        """
+        if (not type(information_object_address) is int) or (information_object_address < 0) or (information_object_address > 16777215):
+            return "ERROR: Information object address has to be an integer between 1 and 16777215."
+        return struct.pack('<3B', information_object_address & 0xFF, (information_object_address >> 8) & 0xFF, (information_object_address >> 16) & 0xFF)
 
 
     def unwrap_message(self):
@@ -279,11 +294,11 @@ class TestWrapper(unittest.TestCase):
 
     def test_asdu_type(self):
         wrapper = IEC104Wrapper()
-        self.assertEqual(b'\x07\x01', wrapper.wrap_asdu_type("M_BO_NA_1"))
-        self.assertEqual(b'\x0d\x01', wrapper.wrap_asdu_type("M_ME_NC_1"))
-        self.assertEqual(b'\x2d\x01', wrapper.wrap_asdu_type("C_SC_NA_1"))
-        self.assertEqual(b'\x64\x01', wrapper.wrap_asdu_type("C_IC_NA_1"))
-        self.assertEqual(b'\x66\x01', wrapper.wrap_asdu_type("C_RD_NA_1"))
+        self.assertEqual(7, wrapper.wrap_asdu_type("M_BO_NA_1"))
+        self.assertEqual(13, wrapper.wrap_asdu_type("M_ME_NC_1"))
+        self.assertEqual(45, wrapper.wrap_asdu_type("C_SC_NA_1"))
+        self.assertEqual(100, wrapper.wrap_asdu_type("C_IC_NA_1"))
+        self.assertEqual(102, wrapper.wrap_asdu_type("C_RD_NA_1"))
         self.assertEqual("ERROR: The ASDU type was not recognized.", wrapper.wrap_asdu_type("test"))
         self.assertEqual("ERROR: The ASDU type was not recognized.", wrapper.wrap_asdu_type(1))
         self.assertEqual("ERROR: The ASDU type was not recognized.", wrapper.wrap_asdu_type(3.4))
@@ -337,22 +352,33 @@ class TestWrapper(unittest.TestCase):
         self.assertEqual("ERROR: Common address has to be an integer between 1 and 65535.", wrapper.wrap_common_address(3.4))
         self.assertEqual("ERROR: Common address has to be an integer between 1 and 65535.", wrapper.wrap_common_address("test"))
 
+    def test_information_object_address(self):
+        wrapper = IEC104Wrapper()
+        self.assertEqual(b'\x00\x00\x00', wrapper.wrap_information_object_address(0))
+        self.assertEqual(b'\x8b\x13\x00', wrapper.wrap_information_object_address(5003))
+        self.assertEqual(b'\xFF\xFF\xFF', wrapper.wrap_information_object_address(16777215))
+        self.assertEqual("ERROR: Information object address has to be an integer between 1 and 16777215.", wrapper.wrap_information_object_address(-1))
+        self.assertEqual("ERROR: Information object address has to be an integer between 1 and 16777215.", wrapper.wrap_information_object_address(44444444443))
+        self.assertEqual("ERROR: Information object address has to be an integer between 1 and 16777215.", wrapper.wrap_information_object_address(3.4))
+        self.assertEqual("ERROR: Information object address has to be an integer between 1 and 16777215.", wrapper.wrap_information_object_address("test"))
+
     def test_asdu(self):
         wrapper = IEC104Wrapper()
-        self.assertEqual(b'\x07\x01\x01\x00\x01\x00', wrapper.wrap_asdu("M_BO_NA_1", ("periodic", 0, 0), 0, 1, "test"))
-        self.assertEqual("ERROR: The ASDU type was not recognized.", wrapper.wrap_asdu(1, ("periodic", 0, 0), 0, 1, "test"))
-        self.assertEqual("ERROR: No cause of transmission was found.", wrapper.wrap_asdu("M_BO_NA_1", ("test", 0, 0), 0, 1, "test"))
-        self.assertEqual("ERROR: Common address has to be an integer between 1 and 65535.", wrapper.wrap_asdu("M_BO_NA_1", ("periodic", 0, 0), 0, -1, "test"))
+        self.assertEqual(b'\x07\x01\x01\x00\x01\x00\x00\x00\x00', wrapper.wrap_asdu("M_BO_NA_1", ("periodic", 0, 0), 0, 1, 0, "test"))
+        self.assertEqual("ERROR: The ASDU type was not recognized.", wrapper.wrap_asdu(1, ("periodic", 0, 0), 0, 1, 0, "test"))
+        self.assertEqual("ERROR: No cause of transmission was found.", wrapper.wrap_asdu("M_BO_NA_1", ("test", 0, 0), 0, 1, 0, "test"))
+        self.assertEqual("ERROR: Common address has to be an integer between 1 and 65535.", wrapper.wrap_asdu("M_BO_NA_1", ("periodic", 0, 0), 0, -1, 0, "test"))
+        self.assertEqual("ERROR: Information object address has to be an integer between 1 and 16777215.", wrapper.wrap_asdu("M_BO_NA_1", ("periodic", 0, 0), 0, 1, -1, "test"))
 
     def test_create_apdu(self):
         wrapper = IEC104Wrapper()
-        self.assertEqual(b'\x02\x00\x02\x00\x07\x01\x01\x00\x01\x00', wrapper.create_apdu("i-frame", "M_BO_NA_1", ("periodic", 0, 0), 0, 1, "test", 1, 1))
-        self.assertEqual("ERROR: No valid frame format was given.", wrapper.create_apdu("test", "M_BO_NA_1", ("periodic", 0, 0), 0, 1, "test", 1, 1))
-        self.assertEqual("ERROR: The ASDU type was not recognized.", wrapper.create_apdu("i-frame", "test", ("periodic", 0, 0), 0, 1, "test", 1, 1))
+        self.assertEqual(b'\x02\x00\x02\x00\x07\x01\x01\x00\x01\x00\x00\x00\x00', wrapper.create_apdu("i-frame", "M_BO_NA_1", ("periodic", 0, 0), 0, 1, 0, "test", 1, 1))
+        self.assertEqual("ERROR: No valid frame format was given.", wrapper.create_apdu("test", "M_BO_NA_1", ("periodic", 0, 0), 0, 1, 0, "test", 1, 1))
+        self.assertEqual("ERROR: The ASDU type was not recognized.", wrapper.create_apdu("i-frame", "test", ("periodic", 0, 0), 0, 1, 0, "test", 1, 1))
 
     def test_create_header(self):
         wrapper = IEC104Wrapper()
-        self.assertEqual(b'\x68\x0a', wrapper.create_apdu_header(b'\x02\x00\x02\x00\x07\x01\x01\x00\x01\x00'))
+        self.assertEqual(b'\x68\x0d', wrapper.create_apdu_header(b'\x02\x00\x02\x00\x07\x01\x01\x00\x01\x00\x00\x00\x00'))
         self.assertEqual("ERROR: APDU too long.", wrapper.create_apdu_header("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, \
             sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et \
             justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimat".encode()))
