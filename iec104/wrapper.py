86a75743fdc943bea430ba76c061ef1561cfc12b
Thomas <thomast3@gmx.de>
2017-06-01 17:50:45 +0200

    Information object
    Not tested yet.

--- iec104/wrapper.py
+++ iec104/wrapper.py
@@ -25,7 +25,7 @@ class IEC104Wrapper():
         """
         Creates a IEC104 APDU header based on the given APDU.
         :param apdu: APDU as bytestring.
-        :return: A struct containing an IEC104 APDU header as bytestring. ERROR if failed.
+        :return: IEC104 APDU header as a bytestring. ERROR if failed.
         """
         if not type(apdu) is bytes:
             return "ERROR: An APDU has to be a bytestring."
@@ -47,7 +47,7 @@ class IEC104Wrapper():
         :param ssn: Send sequence number. This is also used to store relevant information for the U-Frame(has to contain the function name and type: e.g. "test-con").
         :param rsn: Receive sequence number.
         :param originator_address: Originator address as integer.
-        :return: Struct containing an IEC 104 APDU without header as bytestring. ERROR if failed.
+        :return: IEC 104 APDU without header as a bytestring. ERROR if failed.
         """
         apci = self.wrap_frame(frame, ssn, rsn)
         if type(apci) is str:
@@ -59,12 +59,12 @@ class IEC104Wrapper():
 
     def wrap_frame(self, frame, ssn, rsn):
         """
-        Returns a struct containing a IEC 104 frame based on the given frame format.
+        Creates an IEC 104 frame.
         If needed the send sequence number and receive sequence number are used to wrap the frame.
         :param frame: Frame format to be used.
         :param ssn: Send sequence number. This is also used to store relevant information for the U-Frame(has to contain the function name and type: e.g. "test-con").
         :param rsn: Receive sequence number.
-        :return: Struct containing an IEC 104 frame as bytestring. ERROR if failed.
+        :return: IEC 104 frame as a bytestring. ERROR if failed.
         """
         if frame == "i-frame":
             res = self.i_frame(ssn, rsn)
@@ -78,10 +78,10 @@ class IEC104Wrapper():
 
     def i_frame(self, ssn, rsn):
         """
-        Creates struct containing a IEC 104 I-Frame based on the given send sequence number and receive sequence number.
+        Creates an IEC 104 I-Frame.
         :param ssn: Send sequence number.
         :param rsn: Receive sequence number.
-        :return: Struct containing an IEC 104 I-Frame as bytestring. ERROR if failed.
+        :return: IEC 104 I-Frame as a bytestring. ERROR if failed.
         """
         if (not type(ssn) is int) or (ssn < 0) or (ssn > 32767):
             return "ERROR: Send sequence number has to be an integer between 0 and 32767."
@@ -91,9 +91,9 @@ class IEC104Wrapper():
 
     def s_frame(self, rsn):
         """
-        Creates struct containing a IEC 104 S-Frame based on the given receive sequence number.
+        Creates an IEC 104 S-Frame.
         :param rsn: Receive sequence number.
-        :return: Struct containing an IEC 104 S-Frame as bytestring. ERROR if failed.
+        :return: IEC 104 S-Frame as a bytestring. ERROR if failed.
         """
         if (not type(rsn) is int) or (rsn < 0) or (rsn > 32767):
             return "ERROR: Receive sequence number has to be an integer between 0 and 32767."
@@ -101,9 +101,9 @@ class IEC104Wrapper():
 
     def u_frame(self, function):
         """
-        Creates struct containing a IEC 104 U-Frame based on the given information.
+        Creates an IEC 104 U-Frame.
         :param function: Function to be used. Has to contain the function name and type: e.g. "test-con". Defaults to no function being used.
-        :return: Struct containing an IEC 104 U-Frame as bytestring. ERROR if failed.
+        :return: IEC 104 U-Frame as a bytestring. ERROR if failed.
         """
         byte = NO_FUNC
         function = function.lower()
@@ -127,15 +127,17 @@ class IEC104Wrapper():
     
     def wrap_asdu(self, asdu_type, sequence, cause_of_transmission, common_address, message, originator_address = 0):
         """
-        Adds a IEC 104 ASDU to a struct.
+        Creates a IEC 104 ASDU.
         :param asdu_type: Type of the message as string.
         :param sequence: SQ bit as defined in IEC 104.
         :param cause_of_transmission: A tuple containing the cause of transmission as string(e.g. periodic), P/N bit and Testbit.
         :param common_address: Common address of ASDUs as integer.
         :param message: Message to be wrapped. Has to be a list containing less than 128 objects/elements.
         :param originator_address: Originator address as integer.
-        :return: Struct containing an IEC 104 ASDU as bytestring. ERROR if failed.
+        :return: An IEC 104 ASDU as a bytestring. ERROR if failed.
         """
+        if not sequence in [0,1]:
+            return "ERROR: Sequence bit has to be 0 or 1."
         type_id = self.wrap_asdu_type(asdu_type)
         if type(type_id) is str:
             return type_id
@@ -148,10 +150,9 @@ class IEC104Wrapper():
         ca = self.wrap_common_address(common_address)
         if type(ca) is str:
             return ca
-        # io = self.wrap_information_object(vsq, message)
-        ioa = self.wrap_information_object_address()
-        if type(ioa) is str:
-            return ioa
+        io = self.wrap_information_object(type_id, vsq, message)
+        if type(io) is str:
+            return io
         return struct.pack('<2B', type_id, vsq) + cot + ca + ioa
 
     def wrap_asdu_type(self, asdu_type):
@@ -180,7 +181,7 @@ class IEC104Wrapper():
         :param type_id: Type of the message as integer.
         :param sequence: SQ bit as defined in IEC 104.
         :param message: Message to be wrapped. Has to be a list containing less than 128 objects/elements.
-        :return: Struct containing an IEC 104 variable structure qualifier as bytestring. ERROR if failed.
+        :return: Variable structure qualifier as defined in IEC 104 as integer. ERROR if failed.
         """
         if not type(type_id) is int:
              return "ERROR: The type identification has to be an integer."
@@ -200,10 +201,10 @@ class IEC104Wrapper():
 
     def wrap_cause_of_transmission(self, cause_of_transmission, originator_address = 0):
         """
-        Adds a IEC 104 cause of transmission and originator address to a struct.
+        Creates an IEC 104 cause of transmission and and IEC 104 originator address.
         :param cause_of_transmission: A tuple containing the cause of transmission as string(e.g. periodic), P/N bit and Testbit.
         :param originator_address: Originator address as integer.
-        :return: Struct containing an IEC 104 cause of transmission and originator address as bytestring. ERROR if failed.
+        :return: IEC 104 cause of transmission and IEC 104 originator address as a bytestring. ERROR if failed.
         """
         if not type(cause_of_transmission) is tuple:
             return "ERROR: Cause of transmission also needs a P/N bit and Testbit."
@@ -235,9 +236,9 @@ class IEC104Wrapper():
 
     def wrap_common_address(self, common_address):
         """
-        Adds a IEC 104 common address to a struct.
+        Creates an IEC 104 common address.
         :param common_address: Common address of ASDUs as integer.
-        :return: Struct containing an IEC 104 common address as bytestring. ERROR if failed.
+        :return: IEC 104 common address as a bytestring. ERROR if failed.
         """
         if (not type(common_address) is int) or (common_address < 0) or (common_address > 65535):
             return "ERROR: Common address has to be an integer between 0 and 65535."
@@ -245,22 +246,177 @@ class IEC104Wrapper():
 
     def wrap_information_object_address(self):
         """
-        Adds an IEC 104 information object address to a struct.
-        :return: Struct containing an IEC 104 information object address as bytestring. ERROR if failed.
+        Creates an IEC 104 information object address.
+        :return: IEC 104 information object address as a bytestring. ERROR if failed.
         """
         if (not type(self.information_object_address) is int) or (self.information_object_address < 0) or (self.information_object_address > 16777215):
             return "ERROR: Information object address has to be an integer between 0 and 16777215."
         return struct.pack('<3B', self.information_object_address & 0xFF, (self.information_object_address >> 8) & 0xFF, (self.information_object_address >> 16) & 0xFF)
 
+    def wrap_information_object(self, type_id, vsq, message):
+        """
+        Packs the message into the format that is dictated by the type identification.
+        :param type_id: Type of the message as integer.
+        :param vsq: Variable structure qualifier as defined in IEC 104 as integer.
+        :param message: Message to be wrapped. Has to be a list containing less than 128 objects/elements.
+        :return: An information object as defined in IEC 104 as a bytestring. ERROR if failed.
+        """
+        result = b''
+        i = 0
+        if not type(type_id) is int:
+             return "ERROR: The type identification has to be an integer."
+        if not type(vsq) is int:
+             return "ERROR: The variable structure qualifier has to be an integer."
+        if (not type(message) is list) or (len(message) > 127):
+             return "ERROR: The message has to be a list containing less than 128 objects/elements."
+        # Number of objects/elements expected based on the VSQ value
+        length = (vsq & 0x7F)
+        if length == 0:
+            return result
+        if length > len(message):
+            return "ERROR: Variable structure qualifier expects more messages than given."
+        if length < len(message):
+            return "ERROR: Variable structure qualifier expects fewer messages than given."
+        # SQ == 1
+        if (vsq & 0x80) == 0x80:
+            temp = self.wrap_information_object_address()
+            if type(temp) is str:
+                return temp
+            result += temp
+            if type_id == 7:
+                while i < length:
+                    temp = self.wrap_information_object_m_bo_na_1(message[i])
+                    if type(temp) is str:
+                        return temp
+                    result += temp
+                    i -= 1
+            elif type_id == 13:
+                while i < length:
+                    temp = self.wrap_information_object_m_me_nc_1(message[i])
+                    if type(temp) is str:
+                        return temp
+                    result += temp
+                    i -= 1
+            else: 
+                return "ERROR: The ASDU type was not recognized or is not fit to be a sequence of elements."
+        # SQ == 0
+        else:
+            if type_id == 7:
+                while i < length:
+                    temp = self.wrap_information_object_address()
+                    if type(temp) is str:
+                        return temp
+                    result += temp
+                    temp = self.wrap_information_object_m_bo_na_1(message[i])
+                    if type(temp) is str:
+                        return temp
+                    result += temp
+                    i -= 1
+            elif type_id == 13:
+                while i < length:
+                    temp = self.wrap_information_object_address()
+                    if type(temp) is str:
+                        return temp
+                    result += temp
+                    temp = self.wrap_information_object_m_me_nc_1(message[i])
+                    if type(temp) is str:
+                        return temp
+                    result += temp
+                    i -= 1
+            elif type_id == 45:
+                if length != 1:
+                    return "ERROR: C_SC_NA_1 length has to be 1."
+                temp = self.wrap_information_object_address()
+                if type(temp) is str:
+                    return temp
+                result += temp
+                temp = self.wrap_information_object_c_sc_na_1(message)
+                if type(temp) is str:
+                    return temp
+                result += temp
+            elif type_id == 100:
+                if length != 1:
+                    return "ERROR: C_IC_NA_1 length has to be 1."
+                temp = self.wrap_information_object_address()
+                if type(temp) is str:
+                    return temp
+                result += temp
+                temp = self.wrap_information_object_c_ic_na_1(message)
+                if type(temp) is str:
+                    return temp
+                result += temp
+            elif type_id == 102:
+                if length != 1:
+                    return "ERROR: C_RD_NA_1 length has to be 1."
+                temp = self.wrap_information_object_address()
+                if type(temp) is str:
+                    return temp
+                result += temp
+            else:
+                return "ERROR: The ASDU type was not recognized or is not fit to be a sequence of elements."
+        return result
+
+    def wrap_information_object_m_bo_na_1(self, message):
+        """
+        Packs the message into the M_BO_NA_1 format. 
+        :param message: Tuple containing a string and a tuple containing the following bits of the IEC 104 quality descriptor in this order: \
+        blocked, substituted, not topical, invalid. 
+        :return: Message as a bytestring in the M_BO_NA_1 format. ERROR if failed.
+        """
+        if not type(message) is tuple:
+            return "ERROR: M_BO_NA_1 expects a string and a tuple containing some bits of the IEC 104 quality descriptor in a tupel."
+        if not type(message[0]) is str:
+             return "ERROR: M_BO_NA_1 expects a string."
+        overflow = 1 if len(message[0]) > 32 else 0
+        io = self.wrap_quality_descriptor(overflow, message[1][0], message[1][1], message[1][2], message[1][3])
+        if type(io) is str:
+            return io
+        return struct.pack('<4B', message[0]) + io
+
+    def wrap_information_object_m_me_nc_1(self, message):
+        """
+        Packs the message into the M_ME_NC_1 format.
+        :param message: Tuple containing a single float value and a tuple containing the following bits of the quality descriptor in this order: \
+        blocked, substituted, not topical, invalid. 
+        :return: Message as a bytestring in the M_ME_NC_1 format. ERROR if failed.
+        """
+        if not type(message) is tuple:
+            return "ERROR: M_BO_NA_1 expects a float value and a tuple containing some bits of the IEC 104 quality descriptor in a tupel."
+        if not type(message[0]) is float:
+             return "ERROR: M_ME_NC_1 expects a float value."
+        io = self.wrap_quality_descriptor(0, message[1][0], message[1][1], message[1][2], message[1][3])
+        if type(io) is str:
+            return io
+        return struct.pack('<4B', message[0]) + io
+
+    def wrap_information_object_c_sc_na_1(self, message):
+        """
+        Packs the message into the C_SC_NA_1 format.
+        :param message: Tuple containing a single command state bit and a qualifier of command.
+        :return: Message as a bytestring in the C_SC_NA_1 format. ERROR if failed.
+        """
+        if not type(message) is tuple:
+            return "ERROR: C_SC_NA_1 expects a single command state and a qualifier of command in a tupel."
+        io = self.wrap_single_command(message[0], message[1])
+        return io
+        
+    def wrap_information_object_c_ic_na_1(self, message):
+        """
+        Packs the message into the C_IC_NA_1 format.
+        :param message: Number representing an interrogation type as defined in IEC 104.
+        :return: Message as a bytestring in the C_IC_NA_1 format. ERROR if failed.
+        """
+        return self.wrap_qualifier_of_interrogation(message)
+
     def wrap_quality_descriptor(self, overflow, blocked, substituted, not_topical, invalid):
         """
-        Adds a IEC 104 quality descriptor to a struct.
+        Creates an IEC 104 quality descriptor.
         :param overflow: Overflow bit as defined in IEC 104.
         :param blocked: Blocked bit as defined in IEC 104.
         :param substituted: Substituted bit as defined in IEC 104.
         :param not_topical: Not topical bit as defined in IEC 104.
         :param invalid: Invalid bit as defined in IEC 104.
-        :return: Struct containing an IEC 104 quality descriptor as bytestring. ERROR if failed.
+        :return: IEC 104 quality descriptor as a bytestring. ERROR if failed.
         """
         if not overflow in [0,1]:
             return "ERROR: Overflow bit has to be 0 or 1."
@@ -280,10 +436,10 @@ class IEC104Wrapper():
 
     def wrap_single_command(self, single_command_state, qualifier_of_command):
         """
-        Adds a IEC 104 quality descriptor to a struct.
+        Creates an IEC 104 quality descriptor.
         :param single_command_state: Single command state bit as defined in IEC 104.
         :param qualifier_of_command: Qualifier of command as defined in IEC 104. S/E is expected as least significant bit.
-        :return: Struct containing an IEC 104 quality descriptor as bytestring. ERROR if failed.
+        :return: Struct containing an IEC 104 quality descriptor as a bytestring. ERROR if failed.
         """
         if not single_command_state in [0,1]:
             return "ERROR: Single command state bit has to be 0 or 1."
@@ -295,10 +451,10 @@ class IEC104Wrapper():
 
     def wrap_qualifier_of_command(self, qualifier, select_execute):
         """
-        Builds a IEC 104 qualifier of command.
+        Builds an IEC 104 qualifier of command.
         :param overflow: Number representing a command type as defined in IEC 104.
         :param select_execute: S/E bit as defined in IEC 104.
-        :return: An IEC 104 quailfier of command as integer. ERROR if failed.
+        :return: An IEC 104 qualifier of command as integer. ERROR if failed.
         """
         if not select_execute in [0,1]:
             return "ERROR: S/E bit has to be 0 or 1."
@@ -309,9 +465,9 @@ class IEC104Wrapper():
 
     def wrap_qualifier_of_interrogation(self, qualifier):
         """
-        Adds a IEC 104 qualifier of interrogation to a struct.
+        Creates an IEC 104 qualifier of interrogation.
         :param qualifier: Number representing an interrogation type as defined in IEC 104.
-        :return: Struct containing an IEC 104 qualifier of interrogation as bytestring. ERROR if failed.
+        :return: IEC 104 qualifier of interrogation as a bytestring. ERROR if failed.
         """
         if (not type(qualifier) is int) or (qualifier < 0) or (qualifier > 255):
             return "ERROR: Qualifier of interrogation has to be an integer between 0 and 255."
